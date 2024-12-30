8ae6d1266f50d72280069856e090f084f41e48f4
looyer <looyer@sina.com>
2024-12-30 10:42:42 +0800

    1. log info

--- learning/gopackage/go-byexample/go-byfree/main.go
+++ learning/gopackage/go-byexample/go-byfree/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"runtime/debug"
-	"unicode/utf8"
 )
 
 var ErrCold error = fmt.Errorf("can't boiling water ")
@@ -16,6 +14,12 @@ func makeTea(c int32) error {
 		return fmt.Errorf("c err-2:%w", ErrCold) //wrap error is very useful. use errors.Is(e, target) check.
 	}
 	return nil
+}
+
+func showcb(x int32, y int32) {
+	xy := int64(x)<<32 | int64(y)
+	nx, ny := int32(xy>>32), int32(xy&0xFFFFFFFF)
+	log.Printf("ori x:%v y:%v  co-xy:%v  nx:%v ny:%v", x, y, xy, nx, ny)
 }
 
 func main() {
@@ -29,25 +33,38 @@ func main() {
 
 	log.Printf("~~~~~~~~~ go-byfree ~~~~~~~~~")
 
-	stru8 := "hello,你好，！emoji-😀ok."
-	// for k, v := range stru8 {
-	// 	log.Printf("start:%v rune:%v", k, v) //k:rune start index, v:rune
+	xy := int64(-577)
+	log.Printf("h32: %v", int32(xy>>32))
+
+	showcb(20, 31)
+	showcb(35, -13)
+	showcb(-22, 9)
+	showcb(-7, -8)
+
+	showcb(0, 0)
+	showcb(0, -1)
+	showcb(-1, 0)
+	showcb(-1, -1)
+
+	// stru8 := "hello,你好，！emoji-😀ok."
+	// // for k, v := range stru8 {
+	// // 	log.Printf("start:%v rune:%v", k, v) //k:rune start index, v:rune
+	// // }
+	// log.Printf("stru8:%v size:%v runes:%v", stru8, len(stru8), utf8.RuneCountInString(stru8))
+
+	// if e := makeTea(2); e != nil {
+	// 	if errors.Is(e, ErrCold) {
+	// 		log.Printf("can't use cold water to make tea.")
+	// 	}
+	// } else {
+	// 	log.Printf("tea is maked.")
 	// }
-	log.Printf("stru8:%v size:%v runes:%v", stru8, len(stru8), utf8.RuneCountInString(stru8))
 
-	if e := makeTea(2); e != nil {
-		if errors.Is(e, ErrCold) {
-			log.Printf("can't use cold water to make tea.")
-		}
-	} else {
-		log.Printf("tea is maked.")
-	}
+	// //decimal and integer change
+	// var v0 interface{} = int32(5)
+	// fa, ok := v0.(float32)
+	// log.Printf("v0:%v fa:%v ok:%v", v0, fa, ok)
 
-	//decimal and integer change
-	var v0 interface{} = int32(5)
-	fa, ok := v0.(float32)
-	log.Printf("v0:%v fa:%v ok:%v", v0, fa, ok)
-
-	fb := v0.(float32)
-	log.Printf("fb:%v", fb)
+	// fb := v0.(float32)
+	// log.Printf("fb:%v", fb)
 }
--- learning/gopackage/go-grpc/go-message/main.go
+++ learning/gopackage/go-grpc/go-message/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"meproto"
+
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -22,4 +24,25 @@ func main() {
 	err2 := &meproto.ErrorMessage{}
 	json.Unmarshal(jsonstr, err2)
 	log.Printf("err2: %v", err2) //err2: Code:Error_Config ErrorString:"Sorry! Can't find config!"
+
+	msg := &meproto.Building{
+		UUID: 1520001,
+		ID:   101,
+		Factory: &meproto.Factory{
+			ListOK:   []int32{1, 2, 5},
+			ListIn:   []int32{3, 7, 7},
+			DoneTime: 2004,
+		},
+	}
+	jsonstr, _ = json.Marshal(msg)
+	log.Printf("building json:%v", string(jsonstr))
+
+	msg2 := &meproto.Building{}
+	// json.Unmarshal(jsonstr, msg2)
+	protojson.Unmarshal(jsonstr, msg2)
+
+	factory := msg2.GetFactory()
+	land := msg2.GetLand()
+
+	log.Printf("msg2: %v factory:%v land:%v", msg2, factory, land)
 }
